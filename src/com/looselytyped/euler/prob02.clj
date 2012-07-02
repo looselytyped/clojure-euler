9e386f3271652a30e168d5069c7276ba884d8dca
Raju Gandhi <raju.gandhi+github@gmail.com>
2012-07-02 15:53:54 -0400

    Solve Problem 2

---
+++ src/com/looselytyped/euler/prob02.clj
@@ -0,0 +1,12 @@
+(ns
+    ^{:doc "By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms."}
+  com.looselytyped.euler.prob02)
+
+(defn fibonacci
+  []
+  (map first (iterate (fn [[a b]] [b (+ a b)]) [0 1])))
+
+(defn solution
+  []
+  (apply + (filter even? (take-while #(< % 4000000) (fibonacci)))))
+
