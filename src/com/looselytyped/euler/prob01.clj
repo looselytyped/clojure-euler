2c40f2802676ce9057a9e49ad995f3c3144fff19
Raju Gandhi <raju.gandhi+github@gmail.com>
2012-07-02 14:19:31 -0400

    Solve Problem 1

--- src/com/looselytyped/euler/prob01.clj
+++ src/com/looselytyped/euler/prob01.clj
@@ -1,2 +1,13 @@
-(ns com.looselytyped.euler.prob01)
+(ns
+    ^{:doc "Add all the natural numbers below one thousand that are multiples of 3 or 5."}
+  com.looselytyped.euler.prob01)
+
+(defn multiple-of-3-or-5?
+  [n]
+  (or (zero? (mod n 3))
+      (zero? (mod n 5))))
+
+(defn solution
+  []
+  (apply + (filter multiple-of-3-or-5? (range 1000))))
 
